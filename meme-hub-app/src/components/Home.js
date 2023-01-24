bc50b3ad4553cd1062c034d53b8fcfcafa6ae5b9
OyakMasu21 <akzaki17@gmail.com>
2023-01-24 19:43:29 +0300

    Added a couple of other components

--- meme-hub-app/src/components/Home.js
+++ meme-hub-app/src/components/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Navbar from "./Navbar";
 
 function Home(){
     return (
 <div>
     <h1>Meme Hub</h1>
+    <Navbar/>
 </div>
 
     )
---
+++ meme-hub-app/src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Navbar(){
+    return (
+        <div>
+{/* <h2>This is where the navbar will go so use this as an indicator</h2> */}
+        </div>
+    )
+}
+export default Navbar
