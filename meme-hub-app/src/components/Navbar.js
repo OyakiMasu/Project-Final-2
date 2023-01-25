3a5ddb6f4ff0563086fed6150e2b8e291fe20ab0
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-01-25 12:24:31 +0300

    installed dependancies for react-router-dom

--- meme-hub-app/src/components/Navbar.js
+++ meme-hub-app/src/components/Navbar.js
@@ -1,10 +1,13 @@
 import React from "react";
 
+
 function Navbar(){
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
           <div className="container-fluid">
-            
+            {/*<a class="navbar-brand">MEME HUB</a>
+            <a href ="">HOME</a>
+    <a href ="">MEME PAGE</a>*/}
           </div>
         </nav>
     )
