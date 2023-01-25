de2975da5b3912d48bd247590a8666e102ffa81b
OyakMasu21 <akzaki17@gmail.com>
2023-01-25 13:12:42 +0300

    Some changes

--- meme-hub-app/src/App.js
+++ meme-hub-app/src/App.js
@@ -1,4 +1,3 @@
-// import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
 
@@ -6,7 +5,7 @@ import Home from './components/Home';
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Home />
      
 
       </div>
--- meme-hub-app/src/components/Home.js
+++ meme-hub-app/src/components/Home.js
@@ -2,32 +2,42 @@ import React from "react";
 import Navbar from "./Navbar";
 import Login from "./Login";
 import Memepg from "./Memepg";
+import images from "../images/meme1.jpg"
+import image2 from "../images/meme2.webp"
+import images3 from "../images/meme4.jpg"
 
 function Home(){
     
-    return (
+   return (
 <div className="home-container">
+   <h1>Meme Hub</h1>
 
- <h1>Meme Hub</h1>
-
-  <Navbar/>
-  
-    <div className="meme-body" id="home">
-     
+   <Navbar/>
+    <div className="meme-body" id="home"> 
     <p> 
+    <strong id="toptext"> Welcome to the Meme Page Generator !!!!!!!!!! </strong>
 
-    <strong>Have you ever been bored and the only thing that you wanted to do was view memes well this page is all about that.</strong> 
+    <h2> Intro </h2>
+    <p> This is a meme page app which will help you make your day .
+        You will be able to make you family and friends with this app .
+        This page does wonders it may even help you get you're first girlfriend !
+        If you think I am lying stick aroud and find out . 
+        
+    </p>
 
     </p>
-    <p>  <img src="meme1.jpg" alt="water meme"/>  </p>
 
-     <p> <img src="meme2.webp" alt="plane-meme"/> </p>  
+    <div className="pic">
+     <img src={images} alt="water meme"/>
+     <img src={image2} alt="plane-meme"/>
+     <img src={images3} alt="-harrypotter-meme"/>
+    </div>
 
-     <p> <img src="meme4.jpg" alt="-harrypotter-meme"/> </p>     
+    <p>Explore this page by clicking into the Memepg </p>
 
   </div>
-  <Login/>
-  <Memepg/>
+   <Login/>
+   <Memepg/>
    </div>
 
     )
