14d61ed43b716c9c7b0653859991d295c6cccd6c
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-01-25 12:18:16 +0300

    created div container to hold elements in navbar

--- meme-hub-app/src/components/Navbar.js
+++ meme-hub-app/src/components/Navbar.js
@@ -2,9 +2,11 @@ import React from "react";
 
 function Navbar(){
     return (
-        <div>
-{/* <h2>This is where the navbar will go so use this as an indicator</h2> */}
-        </div>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            
+          </div>
+        </nav>
     )
 }
 export default Navbar
