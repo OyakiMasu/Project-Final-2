a9ff311163ec808202b375574d878192066b9c96
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-01-25 20:46:22 +0300

    re-installed dependancies to correct error

--- meme-hub-app/src/App.js
+++ meme-hub-app/src/App.js
@@ -2,20 +2,15 @@
 import './App.css';
 import Home from './components/Home';
 import React from 'react';
-import Navbar from './components/Navbar';
 import { Route, Switch } from "react-router-dom";
 import Login from './components/Login';
-import Memepg from './components/Memepg';
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      
       <Switch>
-      <Route path='/memepg'>
-        <Memepg />
-      </Route>
       <Route path='/login'>
         <Login />
       </Route>
@@ -23,7 +18,7 @@ function App() {
         <Home />
       </Route>
     </Switch>
-    <Home/>
+    
       </div>
   );
 }
