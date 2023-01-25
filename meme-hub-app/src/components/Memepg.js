89764d7509e4ff668c7b6dde26deee5009a1c2be
git_mitchelleokemwa <mitchelleokemwa@gmail.com>
2023-01-25 09:58:53 +0300

    Fetch

--- meme-hub-app/src/components/Memepg.js
+++ meme-hub-app/src/components/Memepg.js
@@ -1,10 +1,41 @@
 import React from "react";
 
-function Memepg(){
-    return (
-        <div>
-            
-        </div>
-    )
-}
-export default Memepg
+function Memepg() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [memes, setMemes] = useState([]);
+  
+    useEffect(() => {
+      fetch("https://imgflip.com/api")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            setIsLoaded(true);
+            setItems(result);
+          },
+         
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        )
+    }, [])
+  
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.id}>
+              {item.name} {item.price}
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  }
+
+  export default Memepg
