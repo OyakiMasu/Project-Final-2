47e1ec1311d8081bc98460f6b0015aa776db3592
Brian <brianmakorious@gmail.com>
2023-01-26 09:52:59 +0300

    done

--- meme-hub-app/src/components/Login.js
+++ meme-hub-app/src/components/Login.js
@@ -1,11 +1,36 @@
 import React from "react";
+import { useHistory } from 'react-router-dom';
+import axios from "axios"
 
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const history = useHistory();
 
-function Login(){
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post('/api/login', { email, password });
+      localStorage.setItem('token', res.data.token);
+      history.push('/memes');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Email:
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <button type="submit">Log in</button>
+    </form>
+  );
+};
+
 export default Login
