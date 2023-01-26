079d64bc8fba75704f533c1af5a12f64e0c44ab3
git_mitchelleokemwa <mitchelleokemwa@gmail.com>
2023-01-26 21:01:14 +0300

    Click me

--- meme-hub-app/src/components/Memepg.js
+++ meme-hub-app/src/components/Memepg.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Memepg(){
+
+     const [memes, setMemes] = useState([{}])
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+        .then(response => response.json())
+        .then(data => setMemes(data.data.memes))
+     }, [])
+    //  console.log(memes)
+
+    const DisplayMemes = memes.map((meme, index) => {
+        return (
+            <div key={index} className="meme-container" >
+             <img src={meme.url} alt="memepic"></img>
+             <h6>{meme.name}</h6>   
+
+            </div>
+        )
+    })
+        const onClickHandler = () => {
+             console.log("getting a meme");
+        };
+
+        return ( 
+          <div>
+          <button onclick={onClickHandler}>Click me for a meme!</button>
+          </div>
+            
+        );
+      
+
+    
     return (
         <div id="MemePg">
+
+            {DisplayMemes}
             
         </div>
     )
