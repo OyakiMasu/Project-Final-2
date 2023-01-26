4f039e7fda0c07dcac2a597dd1f4cef42db2d1d8
git_mitchelleokemwa <mitchelleokemwa@gmail.com>
2023-01-26 12:06:10 +0300

    added

--- meme-hub-app/src/components/Memepg.js
+++ meme-hub-app/src/components/Memepg.js
@@ -1,27 +1,19 @@
 import React, { useEffect, useState } from "react";
 
-const API_URL = "https://imgflip.com/api"
 
 function Memepg() {
-  const [memes, setMemes] = useState(null)
+  const [memes, setMemes] = useState([]);
    
   
     useEffect(() => {
       fetch("https://imgflip.com/api")
-        .then(r => r.json())
-        .then(data => setMemes(data.memes))
-      }, [])
-     
-      if (!memes){
-        return <h2>Loading...</h2>
-      }
+      .then((response) => response.json())
+      .then((data) => {
+        setMemes(data.meme);
+      });
+  }, []);
 
-    return (
-      <div>
-          <p> Funny</p>
-          <img src={memes} alt="Random Meme" />
-      </div>
-    );
+  return <div>{memes.map((meme) => meme.name)}</div>;
   }
 
   export default Memepg;
